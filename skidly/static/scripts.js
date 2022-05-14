68bcbe2d3110c732be1b05db697be41eb5c85a77
Fapsap <90858065+Fapsap@users.noreply.github.com>
2022-05-14 14:17:09 +0500

    Add files via upload

---
+++ skidly/static/scripts.js
@@ -0,0 +1,34 @@
+ozon_btn = document.getElementById("ozon_btn")
+
+ozon_btn.addEventListener( "click" , () =>{
+    link = document.getElementById("ozon_input").value
+    console.log(link)
+    window.open(`https://wa.me/79199941376?text=` + link, '_blank')
+});
+
+
+wild_btn = document.getElementById("wild_btn")
+
+wild_btn.addEventListener( "click" , () =>{
+    link = document.getElementById("wild_input").value
+    console.log(link)
+    window.open(`https://wa.me/79199941376?text=` + link, '_blank')
+});
+
+
+shein_btn = document.getElementById("shein_btn")
+
+shein_btn.addEventListener( "click" , () =>{
+    link = document.getElementById("shein_input").value
+    console.log(link)
+    window.open(`https://wa.me/79199941376?text=` + link, '_blank')
+});
+
+
+yan_btn = document.getElementById("yan_btn")
+
+yan_btn.addEventListener( "click" , () =>{
+    link = document.getElementById("yan_input").value
+    console.log(link)
+    window.open(`https://wa.me/79199941376?text=` + link, '_blank')
+});
